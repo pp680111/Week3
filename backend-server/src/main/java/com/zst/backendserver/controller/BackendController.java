ab2c2149f4e278238603f2e352f2ff151ee084bc
zzsstt <578413616@qq.com>
2021-08-22 11:51:24 +0800

    添加一个后端程序

---
+++ backend-server/src/main/java/com/zst/backendserver/controller/BackendController.java
@@ -0,0 +1,10 @@
+package com.zst.backendserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/backend")
+public class BackendController {
+
+}
